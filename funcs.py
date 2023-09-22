bba150a0f96afcd6d2e103022c154a5fe85072ad
yjb23tech <yjb23tech@gmail.com>
2023-09-22 23:56:48 +0100

    Raikage

--- funcs.py
+++ funcs.py
@@ -2,10 +2,17 @@
 
 def pvp(player_x, player_y, round_counter):
     
-    print(f"Round {round_counter}: FIGHT!")
+    print(f"Round {round_counter + 1}: FIGHT!")
+
+    #Launch atk sequence for the attacking player; launch def sequence for the defending player
+    #Only call funcs once to trigger return of rand generated pwr property; will work with these values 
+    #For duration of the single fight cycle
+    print(f"{player_x.player_name} launches an attack with {player_x.player_launch_atk()} battle points!")
+    print(f"{player_y.player_name} launches a defensive block with {player_y.player_launch_def()} battle points!")
+    #Post func call above, the atk and def should be set and persist for duration of fight cycle
 
     if (player_x.player_atk_pwr > player_y.player_def_pwr):
-        print(f"{player_x.player_name} has broken through {player_y.player_name}'s defenses!")
+        print(f"{player_x.player_name}'s attack has broken through {player_y.player_name}'s defenses!")
         resulting_atk_damage = player_x.player_atk_pwr - player_y.player_def_pwr
         player_y.player_hp = player_y.player_hp - resulting_atk_damage
     
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import player
+import funcs
 
 player_1 = player.Player(player.set_player_name(), player.set_player_location(), player.set_player_age(), player.set_player_special_atk())
 print(player_1)
@@ -14,17 +15,19 @@ while (fight_counter < 5):
 
     if (bool_player_1_starts == True):
         print(f"{player_1.player_name} is attacking!")
-        print(f"{player_2.player_name} is on the defensive\n")
+        print(f"{player_2.player_name} is on the defensive!\n")
+        funcs.pvp(player_1, player_2, fight_counter)
     else:
         print(f"{player_2.player_name} is attacking")
         print(f"{player_1.player_name} is on the defensive!\n")
+        funcs.pvp(player_2, player_1, fight_counter)
 
-    print(f"Attack Power via func call: {player_1.player_launch_atk()}")
-    print(f"Defense Power via func call: {player_1.player_launch_def()}")
-    print(f"HP Status: {player_1.player_hp}")
+    #print(f"Attack Power via func call: {player_1.player_launch_atk()}")
+    #print(f"Defense Power via func call: {player_1.player_launch_def()}")
+    #print(f"HP Status: {player_1.player_hp}")
 
-    print(f"Attack Power via GETish: {player_1.player_atk_pwr}")
-    print(f"Defense Power via GETish: {player_1.player_def_pwr}")
+    #print(f"Attack Power via GETish: {player_1.player_atk_pwr}")
+    #print(f"Defense Power via GETish: {player_1.player_def_pwr}")
 
     print(" ")
 
