7532858aac8d74e7a2564104cdf5df625bdd6337
yjb23tech <yjb23tech@gmail.com>
2023-09-22 16:31:20 +0100

    PyFan

--- main.py
+++ main.py
@@ -3,3 +3,18 @@ import player
 test_player = player.Player("Yondaime", "Konohoa", 29, "Flying God of Death")
 print(test_player)
 
+fight_counter = 0 
+
+while (fight_counter < 5):
+
+    print(f"Attack Power via func call: {test_player.player_launch_atk()}")
+    print(f"Defense Power via func call: {test_player.player_launch_def()}")
+    print(f"HP Status: {test_player.player_hp}")
+
+    print(f"Attack Power via GETish: {test_player.player_atk_pwr}")
+    print(f"Defense Power via GETish: {test_player.player_def_pwr}")
+
+    print(" ")
+    fight_counter += 1
+
+
--- player.py
+++ player.py
@@ -1,3 +1,5 @@
+import random
+
 class Player():
 
     def __init__(self, player_name, player_location, player_age, player_special_atk):
@@ -7,6 +9,7 @@ class Player():
         self.player_age = player_age
         self.player_special_atk = player_special_atk
 
+        self.player_hp = 100
         self.player_atk_pwr = 1
         self.player_def_pwr = 1 
         self.player_special_atk_pwr = 1
@@ -15,5 +18,21 @@ class Player():
         player_details = (f"\nName: {self.player_name}\nLocation: {self.player_location}\nSpecial Atk: {self.player_special_atk}\n")
         return player_details
     
+    def player_hp_status(self):
+        player_hp = self.player_hp
+        return player_hp 
+    
+    def player_launch_atk(self):
+        self.player_atk_pwr = random.randint(1,20)
+        return self.player_atk_pwr
+
+    def player_launch_def(self):
+        self.player_def_pwr = random.randint(1,20)
+        return self.player_def_pwr
+    
+
+
+
+
     
 
