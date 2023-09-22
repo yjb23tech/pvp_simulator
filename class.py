a06f29efa9f61e67fa9b4e2e96cb2170c623e0c0
yjb23tech <yjb23tech@gmail.com>
2023-09-22 16:11:45 +0100

    Avatar

---
+++ class.py
@@ -0,0 +1,7 @@
+class Player():
+    def __init__(self, player_name, player_location, player_age, player_special_atk):
+        
+        self.player_name = player_name
+        self.player_location = player_location
+        self.player_age = player_age
+        self.player_special_atk = player_special_atk
