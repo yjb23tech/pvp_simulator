b751659dd77aad52505bccf4220e9b51638bb86c
yjb23tech <yjb23tech@gmail.com>
2023-09-23 00:46:19 +0100

    Tidying things up

--- main.py
+++ main.py
@@ -1,39 +1,42 @@
 import player
 import funcs
 
-#Create Player 1
-player_1 = player.Player(player.set_player_name(), player.set_player_location(), player.set_player_age(), player.set_player_special_atk())
-print(player_1)
+def play():
 
-#Create Player 2 
-player_2 = player.Player(player.set_player_name(), player.set_player_location(), player.set_player_age(), player.set_player_special_atk())
-print(player_2)
+    #Create Player 1
+    player_1 = player.Player(player.set_player_name(), player.set_player_location(), player.set_player_age(), player.set_player_special_atk())
+    print(player_1)
 
-#Tracks the number of rounds played 
-fight_counter = 0 
+    #Create Player 2 
+    player_2 = player.Player(player.set_player_name(), player.set_player_location(), player.set_player_age(), player.set_player_special_atk())
+    print(player_2)
 
-#Determines who is on attack, who is on defence 
-bool_player_1_starts = True
+    #Tracks the number of rounds played 
+    fight_counter = 0 
 
-while (fight_counter < 5):
+    #Determines who is on attack, who is on defence 
+    bool_player_1_starts = True
 
-    #The Player fed into funcs.pvp 1st is on attack; the Player fed in 2nd is on defence
-    if (bool_player_1_starts == True):
-        #When the bool is True, Player 1 is fed in 1st and Player 2 in 2nd
-        funcs.pvp(player_1, player_2, fight_counter)
-    else:
-        #When the bool is False, Player 2 is passed in 1st and Player 1 in 2nd
-        funcs.pvp(player_2, player_1, fight_counter)
+    while (fight_counter < 5):
 
-    #Once the pvp func is run, the order of the Players needs to swap to allow a new Player on atk and def
-    #Functions as a switch; swaps the order of Players so each Player has the chance to go first 
-    if (bool_player_1_starts == True):
-        print("Player 2 Starts!")
-        bool_player_1_starts = False
-    else:
-        print("Player 1 Starts!")
-        bool_player_1_starts = True 
+        #The Player fed into funcs.pvp 1st is on attack; the Player fed in 2nd is on defence
+        if (bool_player_1_starts == True):
+            #When the bool is True, Player 1 is fed in 1st and Player 2 in 2nd
+            funcs.pvp(player_1, player_2, fight_counter)
+        else:
+            #When the bool is False, Player 2 is passed in 1st and Player 1 in 2nd
+            funcs.pvp(player_2, player_1, fight_counter)
+
+        #Once the pvp func is run, the order of the Players needs to swap to allow a new Player on atk and def
+        #Functions as a switch; swaps the order of Players so each Player has the chance to go first 
+        if (bool_player_1_starts == True):
+            #Player 1 started; order is swapped by changing boolean value; now Player 2 will start
+            bool_player_1_starts = False
+        else:
+            #Player 2 started; order is swapped by changing boolean value; now Player 1 will start 
+            bool_player_1_starts = True 
     
-    fight_counter += 1
+        fight_counter += 1
 
+play()
 
